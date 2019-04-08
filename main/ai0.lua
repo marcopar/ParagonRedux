0fe0dc07c06212634847457edfa95919c78fc3b7
marcopar <marcopar@gmail.com>
2019-04-08 23:00:04 +0200

    AI - level 0

---
+++ main/ai0.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+local CO = require "main.constants"
+local BO = require "main.board"
+local UT = require "main.utils"
+
+M.think = function(boardValues)
+	
+	local tile = vmath.vector3()
+	for x = CO.BOARD_XMIN, CO.BOARD_XMAX do
+		for y = CO.BOARD_YMIN, CO.BOARD_YMAX do
+			tile.x = x;
+			tile.y = y;
+			local cellPos = BO.getCellIndexInBoard(tile)
+			local value = boardValues[cellPos]
+			if(value == nil) then
+				return tile
+			end
+		end
+	end
+	return nil
+end
+
+return M
+	
